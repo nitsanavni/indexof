6137af2fdf375e76cb77c476491dcaf87b49c13c
Nitsan Avni <nitsan@papayaglobal.com>
2018-08-25 08:47:55 +0300

    simplr

--- src/main/groovy/IndexOf.groovy
+++ src/main/groovy/IndexOf.groovy
@@ -1,57 +1,30 @@
 class IndexOf {
 
     private String subString
-    private List<Matcher> matchers = []
+    private String string
 
     static IndexOf indexOf(String subString) {
         new IndexOf(subString: subString)
     }
 
     static void enhanceClass() {
-        String.metaClass.myIndexOf << { subString ->
-            indexOf(subString).inString(delegate as String)
-        }
+        String.metaClass.myIndexOf << { indexOf(it).inString delegate as String }
     }
 
     int inString(String string) {
-        int index = 0
-        Matcher matched
+        this.string = string
 
-        string.any { character ->
-            matchers << new Matcher(index)
-            assert matchers.size() <= subString.size()
-            matched = matchers.find { it.next(character, index) }
-            matchers = matchers.findAll { it.stillHope }
-            index++
-            matched
+        def foundIndex = (0..<string.size()).find { stringIndex ->
+            matchAt(stringIndex)
         }
 
-        matched ? matched.initialIndex : -1
+        foundIndex != null ? foundIndex : -1
     }
 
-    private class Matcher {
-
-        private final initialIndex
-        private boolean stillHope = true
-
-        Matcher(int index) { this.initialIndex = index }
-
-        boolean next(String character, int atIndex) {
-            stillHope = stillHope && stillMatchToSubString(character, atIndex)
-            boolean match = stillHope && reachedEndOfSubString(atIndex)
-            match
+    private boolean matchAt(stringIndex) {
+        (0..<subString.size()).every { subStringIndex ->
+            subString[subStringIndex] == string[stringIndex + subStringIndex]
         }
-
-        private boolean reachedEndOfSubString(int atIndex) {
-            subStringIndex(atIndex) == subString.size() - 1
-        }
-
-        private boolean stillMatchToSubString(String character, int atIndex) {
-            subString.charAt(subStringIndex(atIndex)) == character as char
-        }
-
-        private int subStringIndex(int currentIndex) { currentIndex - initialIndex }
-
     }
 
 }
